ed750ccbba1a13b1ae8f1ad389120f805d17694c
Edson-Oliveira <edson.inet@gmail.com>
2026-09-11 18:13:34 -0300

    Versão 1

--- ERPServerc.dpr
+++ ERPServerc.dpr
@@ -4,6 +4,8 @@ program ERPServerc;
 
 uses
   System.SysUtils,
+  System.Classes,
+  Winapi.Windows, // Necessário para manipular os eventos do console no Windows
   Horse,
   uEntityService in 'src\Services\uEntityService.pas',
   uEntityRepository in 'src\Repositories\uEntityRepository.pas',
@@ -12,9 +14,43 @@ uses
 
 begin
   try
+    // Ignora o CTRL+C e outros sinais de interrupção do console no Windows
+    SetConsoleCtrlHandler(nil, True);
+
+    Writeln('Sistema de ERP - Server (64 Bit) (Release Beta 1 for Windows/64) ', FormatDateTime('dd-mm-yyyy hh:nn:ss', Now));
+    Writeln('');
+
     RegisterRoutes;
 
-    THorse.Listen(9000);
+    TThread.CreateAnonymousThread(
+      procedure
+      var
+        Comando: string;
+      begin
+        while True do
+        begin
+          Readln(Comando);
+
+          if LowerCase(Trim(Comando)) = 'quit' then
+          begin
+            Writeln('Encerrando o servidor...');
+            THorse.StopListen;
+            Break;
+          end
+          else
+          begin
+            Writeln('Comando não reconhecido. Digite "quit" para sair.');
+          end;
+        end;
+      end).Start;
+
+    THorse.Listen(9000,
+    procedure
+    begin
+      Writeln('Servidor rodando na porta 9000.');
+      Writeln('Digite "quit" e pressione ENTER para sair.');
+    end);
+
   except
     on E: Exception do
     begin
@@ -23,3 +59,4 @@ begin
     end;
   end;
 end.
+
